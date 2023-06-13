3defd383b03892a9c5241f55abe1ccd152088eae
harada-nagamine <22S026@ca.harada-gakuen.ac.jp>
2023-06-13 11:32:42 +0900

    0613

--- pages/index.vue
+++ pages/index.vue
@@ -4,6 +4,16 @@
 
 <script>
 export default {
-  name: 'IndexPage'
+  async asyncData(){
+    const { data } = await $axio.get(
+      'https:///i1ck4b91ex.microcms.io/api/vl/blog',
+      {
+        headers: { 'X-MICROCMS-API-KEY': 'iPAu3Flksctvva2qHappfJIramNmEQ2eEF2Z'}
+      }
+    )
+
+    return data
+  }
+  
 }
 </script>
