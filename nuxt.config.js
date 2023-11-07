9d02adcf993acca053443eca8bbc20c19d2ff7d4
harada-nagamine <133723000+harada-nagamine@users.noreply.github.com>
2023-11-07 12:44:09 +0900

    1107

--- nuxt.config.js
+++ nuxt.config.js
@@ -38,6 +38,10 @@ export default {
   modules: ['@nuxtjs/axios'
   ],
 
+  axios: {
+    // proxy: true
+  },
+
   // Build Configuration: https://go.nuxtjs.dev/config-build
   build: {
   }
--- pages/index.vue
+++ pages/index.vue
@@ -1,19 +1,26 @@
 <template>
-  <Tutorial/>
+  <ul>
+    <li v-for="content in contents" :key="content.id">
+      <nuxt-link :to="`/${content.id}`">
+        {{ content.title }}
+      </nuxt-link>
+    </li>
+  </ul>
 </template>
 
 <script>
+import axios from 'axios'
 export default {
-  async asyncData(){
-    const { data } = await $axio.get(
-      'https:///i1ck4b91ex.microcms.io/api/vl/blog',
+  async asyncData() {
+    const { data } = await axios.get(
+      // your-service-id部分は自分のサービスidに置き換えてください
+      'https://i1ck4b91ex.microcms.io/api/v1/blog',
       {
-        headers: { 'X-MICROCMS-API-KEY': 'iPAu3Flksctvva2qHappfJIramNmEQ2eEF2Z'}
+        // your-api-key部分は自分のapi-keyに置き換えてください
+        headers: { 'X-MICROCMS-API-KEY': 'iPAu3Flksctvva2qHappfJIramNmEQ2eEF2Z' }
       }
     )
-
     return data
   }
-  
 }
 </script>
